de0843262db47eaff299c46a1989a9c9697542cc
Aydar <gareev@aydar.me>
2017-02-15 21:19:00 +0200

    fix close4wiki script

--- closedbeta2wiki/main.go
+++ closedbeta2wiki/main.go
@@ -1,20 +1,65 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
 
-	config "github.com/agareev/MoexLib/other"
 	confluence "github.com/seppestas/go-confluence"
 )
 
+var (
+	path     = "./file.txt"
+	pageid   = "http://url.com"
+	login    = "login"
+	password = "password"
+)
+
+func readFile(path string) string {
+	bs, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalln(err)
+		return "unknown error"
+	}
+	str := string(bs)
+	return str
+}
+
+func splitFile(body string) []string {
+	bodys := strings.Split(body, "\n\n")
+	return bodys
+}
+
+func parseFile(bodys []string) string {
+	body := strings.Join(bodys, "\n-------------\n")
+	return body
+}
+
+// data := url.Values{}
+// 	data.Set("expand", strings.Join(expand, ","))
+// 	contentEndPoint.RawQuery = data.Encode()
+
+func push2wiki(contentID, login, password string) {
+	z := confluence.BasicAuth(login, password)
+	// pageid - url2wiki
+	x, _ := confluence.NewWiki(pageid, z)
+	expand := make([]string, 1)
+	expand = append(expand, "title")
+	expand = append(expand, "body")
+	f, _ := x.GetContent(contentID, expand)
+	fmt.Println(f)
+}
+
 func main() {
-	var configuration config.Config
-	configuration = config.ReadConfig("confg.json")
-	z := confluence.BasicAuth("myname", "mypass")
-	x, _ := confluence.NewWiki("http://wiki.myurl.com", z)
-	ff := make([]string, 3)
-	ff[0] = "sss"
-	f, _ := x.GetContent("47646880", ff)
-	fmt.Println(f.Body)
-	fmt.Println(configuration.Server)
+	flag.StringVar(&path, "f", path, "please use the file")
+	flag.StringVar(&login, "l", login, "login")
+	flag.StringVar(&password, "p", password, "password")
+	flag.Parse()
+
+	// x := splitFile(readFile(path))
+	// fmt.Println(parseFile(x))
+	push2wiki("47646880", login, password)
+
 }
