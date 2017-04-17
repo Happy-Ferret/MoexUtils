3ad741ed29c7c155f75ee494a856fa89e2bad02a
Aydar Gareev <aydar.gareev@moex.com>
2017-04-17 17:47:50 +0300

    add skel
    Signed-off-by: Aydar Gareev <aydar.gareev@moex.com>

--- HaproxyStats/main.go
+++ HaproxyStats/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	moexlib "github.com/agareev/MoexLib/monitoring"
+)
+
+func extractCSV(state []byte) map[string]bool {
+	//TODO extract haproxy CSV
+	var x map[string]bool
+	return x
+}
 
 func main() {
 	fmt.Println("a")
+	moexlib.GetAllContents("http://127.0.0.1:9300")
+	// extractCSV
+	//moexlib.Send2Graphite(delta, key, graphiteHost, graphitePort)
 }
