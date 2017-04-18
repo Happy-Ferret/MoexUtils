3ba40e24ba912b1cfd5b37787440fe3a445fe578
Aydar Gareev <aydar.gareev@moex.com>
2017-04-18 15:35:01 +0300

    add csv parsing
    Signed-off-by: Aydar Gareev <aydar.gareev@moex.com>

--- HaproxyStats/main.go
+++ HaproxyStats/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
+	"strings"
 
 	moexlib "github.com/agareev/MoexLib/monitoring"
 )
 
 func extractCSV(state []byte) map[string]bool {
 	//TODO extract haproxy CSV
+	a := string(state)
+	r := csv.NewReader(strings.NewReader(a))
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(record[1], record[17])
+	}
+
 	var x map[string]bool
 	return x
 }
 
 func main() {
 	fmt.Println("a")
-	moexlib.GetAllContents("http://127.0.0.1:9300")
+	m := moexlib.GetAllContents("http://127.0.0.1:9300/haproxy?stats;csv")
+	extractCSV(m)
 	// extractCSV
 	//moexlib.Send2Graphite(delta, key, graphiteHost, graphitePort)
 }
