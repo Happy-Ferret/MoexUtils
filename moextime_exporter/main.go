9a2399253e8195ec08d390a4299f9f1a0a87f00c
Aydar <gareev@aydar.me>
2017-07-14 17:55:55 +0300

    add tradetime exporter
    Signed-off-by: Aydar <gareev@aydar.me>

--- moextime_exporter/main.go
+++ moextime_exporter/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 var (
-	listenPort = ":9262"
+	listenPort = "9262"
 )
 
 func init() {
@@ -14,6 +18,36 @@ func init() {
 	flag.Parse()
 }
 
+func setTradeTime() float64 {
+	t := time.Now()
+	cliringStart := time.Date(t.Year(), t.Month(), t.Day(), 14, 0, 0, 0, t.Location())
+	cliringStop := time.Date(t.Year(), t.Month(), t.Day(), 14, 5, 0, 0, t.Location())
+	tradesStop := time.Date(t.Year(), t.Month(), t.Day(), 18, 45, 0, 0, t.Location())
+	if t.After(cliringStart) && t.Before(cliringStop) {
+		return 0
+	} else if t.After(tradesStop) {
+		return 0
+	}
+	return 1
+}
+
 func main() {
-	log.Println(listenPort)
+	tradeTime := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "times",
+		Help: "times description",
+	})
+	prometheus.MustRegister(tradeTime)
+
+	go func() {
+		for {
+			tradeTime.Set(setTradeTime())
+			time.Sleep(time.Duration(60 * time.Second))
+			log.Println(setTradeTime())
+		}
+	}()
+
+	http.Handle("/metrics", prometheus.Handler())
+
+	http.ListenAndServe(":"+listenPort, nil)
+
 }
